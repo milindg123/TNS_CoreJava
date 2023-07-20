090953a82979760228a22d14876a47ca367e614e
Milind Ghegadmal <97649757+milindg123@users.noreply.github.com>
2023-07-20 22:47:19 +0530

    Commit

---
+++ Day02/src/org/tnsif/BasicJava/Question2.java
@@ -0,0 +1,13 @@
+package org.tnsif.BasicJava;
+import java.util.Scanner;
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.nextLine();
+		System.out.println("Hello " + name + " have a good day");
+	}
+
+}
