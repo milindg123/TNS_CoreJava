8be1e97f7c71246bd29e650b22e7c6c170478a90
Milind Ghegadmal <97649757+milindg123@users.noreply.github.com>
2023-07-20 22:27:08 +0530

    Second Commit

---
+++ Day02/src/org/tnsif/BasicJava/Variables_Datatypes.java
@@ -0,0 +1,22 @@
+package org.tnsif.BasicJava;
+import java.util.Scanner;
+public class Variables_Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("The int number is:");
+		int num1 = sc.nextInt();
+		System.out.println(num1);
+		
+		System.out.println("The float number is:");
+		float num2 = sc.nextFloat();
+		System.out.println(num2);
+		
+		System.out.println("The short number is:");
+		short num3 = sc.nextShort();
+		System.out.println(num3);
+		
+	}
+
+}
