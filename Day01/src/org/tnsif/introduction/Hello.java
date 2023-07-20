d0cc023d2c6613fe40c06f825487044ae6c00e7c
Milind Ghegadmal <97649757+milindg123@users.noreply.github.com>
2023-07-20 21:40:05 +0530

    First_Commit

---
+++ Day01/src/org/tnsif/introduction/Hello.java
@@ -0,0 +1,10 @@
+package org.tnsif.introduction;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
---
+++ Day01/src/org/tnsif/introduction/UserInput1.java
@@ -0,0 +1,14 @@
+package org.tnsif.introduction;
+import java.util.Scanner;
+public class UserInput1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.nextLine();
+		System.out.println(name);
+		
+	}
+
+}
